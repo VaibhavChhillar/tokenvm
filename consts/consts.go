902bcb3b6af0a927361d8a905035da7346018d07
Vaibhav Chhillar <131567808+VaibhavChhillar@users.noreply.github.com>
2024-07-31 20:50:39 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "vaibhav chhillar"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Vaibhav"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "vbv"
 )
 
 var ID ids.ID
